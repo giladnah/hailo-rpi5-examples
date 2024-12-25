f88411ce1e08af734f2b813d26928bf93f907f4b
Nir Abramovich <nira@hailo.ai>
2024-12-25 18:59:33 +0200

    Squash merge server-parse-commands into team_navi

--- community_projects/Navigaitor/server/main.py
+++ community_projects/Navigaitor/server/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 import uvicorn
+import json
+from server.move import move
 
 
 app = FastAPI()
@@ -11,3 +13,13 @@ async def root():
 def start():
     uvicorn.run("server.main:app", host="0.0.0.0", port=8000, reload=True)
 
+
+
+
+# expecting: json in the format of `{"pressed" or "released": "forward" or "backward" or "left" or "right"}`
+@app.websocket("/move")
+async def move_robot(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        move(json.loads(data))
